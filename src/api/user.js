e23d8239736695c1d4e2444adb07a555b4c85192
huayecai <hakusin1717@163.com>
2022-02-04 13:59:03 +0800

    项目实现-首页文章列表

--- src/api/user.js
+++ src/api/user.js
@@ -29,3 +29,13 @@ export const getUserInfo = () => {
     // }
   })
 }
+// 获取频道列表
+export const getUserChannels = () => {
+  return request({
+    method: 'GET',
+    url: '/v1_0/user/channels'
+    // headers: {
+    //   Authorization: `Bearer ${store.state.user.token}`
+    // }
+  })
+}
